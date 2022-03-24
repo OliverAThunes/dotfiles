88cdce54b7e31509744f314dae5f892a88d1bb07
Oliver Andreas Thunæs <oliver@netron.no>
2022-03-24 16:43:01 +0100

    Redid the entire thing

--- config/dunst/dtest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pidof dunst && killall dunst
-dunst &
-notify-send -u critical "Oh no"
-sleep 1
-notify-send -u normal "Our table"
-sleep 1
-notify-send -u low "It's broken"
---
+++ nvim/.config/nvim/nvim/ginit.vim
@@ -0,0 +1,5 @@
+" Set font
+":GuiFont! DejaVu\ Sans\ Mono\ for\ Powerline:h9
+":GuiFont! fixedsys:h9
+":GuiFont! Source\ Code\ Pro:h11
+
