ea50830724f51aee867b4015002d034ae0c135da
Oliver Andreas Thunæs <oliver.thunaes@gmail.com>
2022-04-29 16:04:50 +0200

    RICE

--- i3/.config/i3/launch_polybar.sh
+++ i3/.config/i3/launch_polybar.sh
@@ -2,10 +2,13 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d " " -f1); do
-        MONITOR=$m polybar --reload top &
-    done
-else
-    polybar --reload top &
-fi
+CONFIG_PATH="/home/oliver/.config/polybar/config.ini"
+
+
+#if type "xrandr"; then
+    #for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        #MONITOR=$m polybar --config=$CONFIG_PATH --reload top &
+    #done
+#else
+    #polybar --reload --config=$CONFIG_PATH main &
+#fi
